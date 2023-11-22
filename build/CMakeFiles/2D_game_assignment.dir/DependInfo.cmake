38794528390c774d38c617c3e216fd6abf2cc279
tatosf <santiagofischel1@gmail.com>
2023-11-22 16:34:09 +0100

    cmake build

---
+++ build/CMakeFiles/2D_game_assignment.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/santiagofischel/Desktop/University/Programming 2/C++/2D-game/src/game.cpp" "CMakeFiles/2D_game_assignment.dir/src/game.cpp.o" "gcc" "CMakeFiles/2D_game_assignment.dir/src/game.cpp.o.d"
+  "/Users/santiagofischel/Desktop/University/Programming 2/C++/2D-game/src/main.cpp" "CMakeFiles/2D_game_assignment.dir/src/main.cpp.o" "gcc" "CMakeFiles/2D_game_assignment.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/2D_game_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "2D_game_assignment"
+  "2D_game_assignment.pdb"
+  "CMakeFiles/2D_game_assignment.dir/src/game.cpp.o"
+  "CMakeFiles/2D_game_assignment.dir/src/game.cpp.o.d"
+  "CMakeFiles/2D_game_assignment.dir/src/main.cpp.o"
+  "CMakeFiles/2D_game_assignment.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2D_game_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
